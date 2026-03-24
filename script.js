15ac9e956b249c0dda36bbef1bc115ba23162f37
Angel <etti.joseangel@gmail.com>
2026-03-23 19:42:42 -0600

    Segunda instruccion realizada

--- script.js
+++ script.js
@@ -5,19 +5,13 @@ const productos = [
     { nombre: "Camiseta", precio: 15, categoria: "Ropa" },
     { nombre: "Laptop", precio: 800, categoria: "Electrónica" },
     { nombre: "Libro", precio: 12, categoria: "Educación" },
-    { nombre: "Zapatos", precio: 50, categoria: "Ropa" },
+    { nombre: "Agua", precio: 50, categoria: "Ropa" },
     { nombre: "Celular", precio: 600, categoria: "Electrónica" },
 ];
 
 // 2. Usa filter() para obtener los productos que cuesten menos de $100
-const menos_cien = productos
-    .filter(producto => producto.precio < 100)
-    .map(producto => producto.nombre);
+const menos_cien = productos.filter(producto => producto.precio < 100);
 
 console.log("Los productos que valen menos de 100 pesos son: ", menos_cien);
 console.log("****************");
 
-// 3. Usa sort() para ordenar esos productos alfabeticamente por su nombre
-const ordenar_productos = menos_cien.sort((a, b) => a - b);
-
-console.log("Los productos ordenados son: ", ordenar_productos);
